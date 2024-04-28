261f72d34c75769959a23a9861db8a372bf24f17
miguel antonio liranzo zabala <miguel2012000@hotmail.com>
2024-04-28 15:12:47 -0700

    update code

--- Tarea1_calcular/Class/Calcualar.cs
+++ Tarea1_calcular/Class/Calcualar.cs
@@ -6,26 +6,36 @@ namespace Tarea1_calcular.Class
     {
         public void secuencial()
         {
+
             int num1 = 0;
             int num2 = 0;
             int suma = 0;
             int producto = 0;
             int resultado = 0;
 
-            Console.WriteLine("Digita el primer numero:");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            try {
+                Console.WriteLine("Digita el primer numero:");
+                num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digita el segundo numero:");
-            num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digita el segundo numero:");
+                num2 = Convert.ToInt32(Console.ReadLine());
 
-            suma = (num1 + num2);
-            producto = (num1 * num2);
+                suma = (num1 + num2);
+                producto = (num1 * num2);
+
+
+                Console.WriteLine($"El total de la suma es : {suma}, y el resultado del producto es : {producto} ");
+
+            }
+            catch (Exception ex){
+                Console.WriteLine($"Ocurrio un erro al intentar calcular el producto: {ex}");
+
             
 
-            Console.WriteLine($"El total de la suma es : {suma }, y el resultado del producto es : {producto} ");
+            }
 
-            
-    
+
+
 
 
         }
